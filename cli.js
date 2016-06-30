89cfc9aace67cd513f0cf75b8130f8b18827bda6
Pelle Wessman <pelle@kodfabrik.se>
2016-06-30 21:39:09 +0200

    Added CLI options

--- cli.js
+++ cli.js
@@ -1,19 +1,72 @@
 #!/usr/bin/env node
+'use strict';
 
 const chalk = require('chalk');
+const dashdash = require('dashdash');
+
 const installedCheck = require('./');
 
-// TODO: Enable configuration of options
-installedCheck('.', {
-  engineCheck: true
+const options = [
+  {
+    names: ['help', 'h'],
+    type: 'bool',
+    help: 'Print this help and exit.'
+  },
+  {
+    names: ['check-engine', 'e'],
+    type: 'bool',
+    help: 'Checks that the engine requirements of the main package is compatible that of its dependencies.'
+  },
+  {
+    names: ['strict', 's'],
+    type: 'bool',
+    help: 'Treat warnings as errors.'
+  },
+  {
+    names: ['verbose', 'v'],
+    type: 'bool',
+    help: 'Shows warnings and notices.'
+  }
+];
+
+const parser = dashdash.createParser({ options });
+
+let opts;
+
+try {
+  opts = parser.parse(process.argv);
+} catch (e) {
+  console.error(chalk.bgRed('Error:'), e.message);
+  process.exit(1);
+}
+
+if (opts.help) {
+  const help = parser.help().trimRight();
+  console.log(
+    '\n' +
+    'Usage: installed-check <path to module folder>\n\n' +
+    'Defaults to current folder.\n\n' +
+    'Options:\n' +
+    help +
+    '\n'
+  );
+  process.exit(0);
+}
+
+installedCheck(opts._args[0], {
+  engineCheck: opts.check_engine
 }).then(result => {
+  if (opts.strict) {
+    result.errors = result.warnings.concat(result.errors);
+    result.warnings = [];
+  }
   if (Object.keys(result).some(key => result[key].length)) {
     console.log('');
   }
-  if (result.notices.length) {
+  if (opts.verbose && result.notices.length) {
     console.log(chalk.blue('Dependency notices:') + '\n\n' + result.notices.join('\n') + '\n');
   }
-  if (result.warnings.length) {
+  if (opts.verbose && result.warnings.length) {
     console.log(chalk.yellow('Dependency warnings:') + '\n\n' + result.warnings.join('\n') + '\n');
   }
   if (result.errors.length) {
