2e934a7f7d3ba6a5da4357576ead34a1f08cd79b
Pelle Wessman <pelle@kodfabrik.se>
2022-07-04 20:21:49 +0200

    Mark as a ESM module

---
+++ cli-wrapper.cjs
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+'use strict';
+
+require('version-guard')('./cli.js', 14, 18);
--- cli-wrapper.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-'use strict';
-
-require('version-guard')('./cli.mjs', 14, 18);
