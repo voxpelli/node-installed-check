a2985941f541071365785008784f3b335dc72da3
Pelle Wessman <pelle@kodfabrik.se>
2022-07-04 21:46:59 +0200

    Use top level wait

--- cli.js
+++ cli.js
@@ -1,7 +1,5 @@
 /* eslint-disable promise/always-return, no-console, unicorn/no-process-exit */
 
-'use strict';
-
 import chalk from 'chalk';
 import meow from 'meow';
 import { messageWithCauses, stackWithCauses } from 'pony-cause';
@@ -93,28 +91,28 @@ const hasNonEmptyProperties = (obj) => {
   return false;
 };
 
-installedCheck(checkOptions).then(result => {
-  if (strict) {
-    result.errors = [...result.warnings, ...result.errors];
-    result.warnings = [];
-  }
-  if (
-    result.errors.length ||
-    (verbose && hasNonEmptyProperties(result))
-  ) {
-    console.log('');
-  }
-  if (verbose && result.notices.length) {
-    console.log(chalk.blue('Dependency notices:') + '\n\n' + result.notices.join('\n') + '\n');
-  }
-  if (verbose && result.warnings.length) {
-    console.log(chalk.yellow('Dependency warnings:') + '\n\n' + result.warnings.join('\n') + '\n');
-  }
-  if (result.errors.length) {
-    console.error(chalk.bgRed('Dependency errors:') + '\n\n' + result.errors.join('\n') + '\n');
-    process.exit(1);
-  }
-}).catch(err => {
+const result = await installedCheck(checkOptions).catch(err => {
   console.error(chalk.bgRed('Unexpected error:') + ' ' + messageWithCauses(err) + '\n\n' + stackWithCauses(err) + '\n');
   process.exit(1);
 });
+
+if (strict) {
+  result.errors = [...result.warnings, ...result.errors];
+  result.warnings = [];
+}
+if (
+  result.errors.length ||
+  (verbose && hasNonEmptyProperties(result))
+) {
+  console.log('');
+}
+if (verbose && result.notices.length) {
+  console.log(chalk.blue('Dependency notices:') + '\n\n' + result.notices.join('\n') + '\n');
+}
+if (verbose && result.warnings.length) {
+  console.log(chalk.yellow('Dependency warnings:') + '\n\n' + result.warnings.join('\n') + '\n');
+}
+if (result.errors.length) {
+  console.error(chalk.bgRed('Dependency errors:') + '\n\n' + result.errors.join('\n') + '\n');
+  process.exit(1);
+}
